95a508d25b36c17d79c825f9389a9f9b2a96a9e9
98765432a <127280386+98765432a@users.noreply.github.com>
2023-03-14 11:22:27 +0800

    Create variables.py

---
+++ Practical5/variables.py
@@ -0,0 +1,7 @@
+a=-3.19
+b=-118.24
+c=116.39
+d=a-b #Calculate	the	longitude	distance	that	Rob	travelled	to	Los	Angeles
+e=c-a #Calculate	the	longitude	distance	that	Rob	travelled	to	Haining
+d>e
+False
