00bafb20e43781faf3559c01134214f19fc54f4d
98765432a <127280386+98765432a@users.noreply.github.com>
2023-03-14 11:33:05 +0800

    Update variables.py

--- Practical5/variables.py
+++ Practical5/variables.py
@@ -5,3 +5,22 @@ d=a-b #Calculate	the	longitude	distance	that	Rob	travelled	to	Los	Angeles
 e=c-a #Calculate	the	longitude	distance	that	Rob	travelled	to	Haining
 d>e
 False
+
+
+>>> x=True
+>>> y=False
+>>> w=x and y
+>>> bool("w")
+True
+>>> x and y
+False
+>>> bool(w)
+False
+>>> x=1>=1
+>>> y=5>6
+>>> w=x and y
+>>> bool(w)
+False
+>>> z=x or y
+>>> bool(z)
+True
