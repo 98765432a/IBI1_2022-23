6caf6514e21ee9c7614c6619cad5d6f7d5866464
WD <wd@WDdeMacBook-Air.local>
2023-03-17 16:26:28 +0800

    final2

--- Practical5/breed_like_rabbits.py
+++ Practical5/breed_like_rabbits.py
@@ -1,6 +1,15 @@
-n=1
-while 1==1:
-        total=2**n
+#each two rabbits generate 2 rabbits
+#the first generation is a pair of rabbit
+#count the total number of rabbits of each generation
+#increase the generation
+#loop
+#if the total number of rabbits is bigger than 100
+#exit loop
+#print out the generation
+
+n=1#the generation of rabbits
+while 1==1:#to make a loop
+        total=2**n #the total number of rabbits=2^n
         n=n+1
 	if total>100:
         	print(n-1)
