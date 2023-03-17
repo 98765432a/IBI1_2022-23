9b0702a119e05026a036c29059852249f1778ae3
98765432a <127280386+98765432a@users.noreply.github.com>
2023-03-17 16:29:23 +0800

    Update variables.py

--- Practical5/variables.py
+++ Practical5/variables.py
@@ -2,7 +2,7 @@ a=-3.19
 b=-118.24
 c=116.39
 d=a-b #Calculate	the	longitude	distance	that	Rob	travelled	to	Los	Angeles
-e=c-a 
+e=c-a #Calculate	the	longitude	distance	that	Rob	travelled	to	Haining
 x=d>e
 bool(x)
 print(x)
