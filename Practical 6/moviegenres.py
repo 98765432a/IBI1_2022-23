e229af0b3b83972ecfa9a79323951c0a119cb32e
WD <wd@WDdeMacBook-Air.local>
2023-03-22 13:26:05 +0800

    first1

--- Practical 6/moviegenres.py
+++ Practical 6/moviegenres.py
@@ -1,12 +1,12 @@
 movie={'Comedy':73,'Action':42,'Romance':38,'Fantasy':28,'Science-fiction':22,'Horror':19,'Crime':18,'Documentary':12,'History':8,'War':7}
+#creat a dictionary,movie genre as index,No.	students	for	which	this genre	is	their	favourite as element
 import numpy as np
-import matplotlib.pyplot as plt
-#make a pie chart
-labels='Comedy','Action','Romance','Fantasy','Science-fiction','Horror','Crime','Documentary','History','War'
-sizes=[73,42,38,28,22,19,18,12,8,7]
-plt.pie(sizes,labels=labels,autopct='%1.1f%%',shadow=False,startangle=90)#the style of the pie chart
+import matplotlib.pyplot as plt #import python packages
+labels='Comedy','Action','Romance','Fantasy','Science-fiction','Horror','Crime','Documentary','History','War'#import the labels
+sizes=[73,42,38,28,22,19,18,12,8,7]#import the value of the labels accordingly
+plt.pie(sizes,labels=labels,autopct='%1.1f%%',shadow=False,startangle=90)#make a pie chart,and define the style of the pie chart
 plt.axis('equal')#ensure that pie is drawn as a circle.
-plt.show()
-a='Comedy'
-print(movie[a])
+plt.show()#print the pie chart
+x='Comedy'
+print(movie[x])#the number of students who prefer Comedy(a movie genre) from the list
 
