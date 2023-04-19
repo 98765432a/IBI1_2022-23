4d4b73a6f22fe4d190b98c6c46c50d1437a5b9b4
WD <wd@WDdeMacBook-Air.local>
2023-04-19 14:41:13 +0800

    third

--- Practical 6/moviegenres.py
+++ Practical 6/moviegenres.py
@@ -1,9 +1,9 @@
 movie={'Comedy':73,'Action':42,'Romance':38,'Fantasy':28,'Science-fiction':22,'Horror':19,'Crime':18,'Documentary':12,'History':8,'War':7}
-#creat a dictionary,movie genre as index,No.	students	for	which	this genre	is	their	favourite as element
+#creat a dictionary,movie genre as index,No.students for which this genre is their favourite as element
 import numpy as np
 import matplotlib.pyplot as plt #import python packages
-labels='Comedy','Action','Romance','Fantasy','Science-fiction','Horror','Crime','Documentary','History','War'#import the labels
-sizes=[73,42,38,28,22,19,18,12,8,7]#import the value of the labels accordingly
+labels=list(movie.keys())#import the labels
+sizes=list(movie.values())#import the value of the labels accordingly
 plt.pie(sizes,labels=labels,autopct='%1.1f%%',shadow=False,startangle=90)#make a pie chart,and define the style of the pie chart
 plt.axis('equal')#ensure that pie is drawn as a circle.
 plt.show()#print the pie chart
