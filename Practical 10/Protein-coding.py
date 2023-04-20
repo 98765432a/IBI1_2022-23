33bcb80d20b43d10ab9c125018d7e88d951c12dd
WD <wd@WDdeMacBook-Air.local>
2023-04-20 16:23:20 +0800

    fifth

--- Practical 10/Protein-coding.py
+++ Practical 10/Protein-coding.py
@@ -3,7 +3,7 @@ DNA='GGgATgcccccctgaAAAT'#an example, which is supposed to be protein-coding
 #DNA=input('input a DNA sequence here:')#use this if we need to input DNA sequence
 coding=DNA.upper()#change the DNA sequence into upper class
 num_coding=len(coding)#calculate how many letters in the input DNA sequence
-extract=str(re.findall(r'ATG\S+TGA',coding))#extract the coding sequence and change the type into string
+extract=str(re.findall(r'ATG(\S+)TGA',coding))#extract the coding sequence and change the type into string
 num_extract=len(extract)#calculate how many letters in the input DNA sequence
 class Coordinate(object):
 	def __init__(self,num_coding,num_extract):#Initialize function
