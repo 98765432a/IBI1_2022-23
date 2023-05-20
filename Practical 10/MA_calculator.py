8b393075d79bfc99c0202ecf53fa68fb8e1969b1
WD <wd@WDdeMacBook-Air.local>
2023-05-20 16:01:50 +0800

    finall1

--- Practical 10/MA_calculator.py
+++ Practical 10/MA_calculator.py
@@ -1,18 +1,13 @@
-class Coordinate (object):#import a class
-	def __init__(self,house,salary):#import a function
-		"""define house,salary
-		calculate how many times the price of house is higher than the annual salary
-		if the calculation is bigger than 5
-		then we can buy the house""" 
-		print('whether buy a house in the UK')
-		self.house=house
-		self.salary=salary
-		buy=self.house/self.salary#calculate how many times the price of house higher than annual salary
-		if buy<5:
-			print('Yes')
-		else:
-			print('No')
-c=Coordinate(180000, 35000)	
-housep=input('please enter the house price here:')#input data
-salarym=input('please enter your annual salary here:')#input data
-c=Coordinate(int(housep),int(salarym))#change the input into intergers
+def calculate(house,salary):
+	"""calculate how many times the price of house is higher than the annual salary
+        if the calculation is bigger than 5
+        then we can buy the house"""
+	print('whether buy a house in the UK')
+	if (house/salary)<5:#calculate if the price of house is less than five times of salary
+		print('Yes')
+	else:
+		print('No')
+c=calculate(180000,3500)#input house price and salary.for example,house price=180000,salary=35000,he/she can't bind the house
+#housep=input('please enter the house price here:')#input data
+#salarym=input('please enter your annual salary here:')#input data
+#c=Coordinate(int(housep),int(salarym))#change the input into intergers
