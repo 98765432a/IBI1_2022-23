ecf34586c8e2894f4242d3cfa339b8191ce6a169
98765432a <127280386+98765432a@users.noreply.github.com>
2023-05-22 21:50:52 +0800

    hexagons.py

--- Practical5/hexagons.py
+++ Practical5/hexagons.py
@@ -1,6 +1,6 @@
-
-n=6
-for i in range (1,6): #let n from 1 to 5
-	n=n-1
-	h=2*n*(2*n-1)/2  #formula
+#calculate the first 5 numbers of distinct dots in a pattern of dots consisting of the outlines of regular hexagons over the increasing number of regular hexagons 
+n=0#n=the number of regular hexagons
+for i in range (1,6): #the for loop was repeated 5 times
+	n=n+1#let the number of regular hexagons ascend from 1 to 5
+	h=2*n*(2*n-1)/2  #calculate the number of distinct dots in a pattern of dots consisting of the outlines of regular hexagons
 	print(h)  #print out the outcomes
