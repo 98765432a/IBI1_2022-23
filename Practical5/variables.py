a2ec30307bc9df1433178fa74ace6fa1c7516c78
98765432a <127280386+98765432a@users.noreply.github.com>
2023-05-22 21:55:05 +0800

    variables.py

--- Practical5/variables.py
+++ Practical5/variables.py
@@ -3,8 +3,8 @@
 a=-3.19#the longitude of Edinburgh
 b=-118.24#the longitude of Los Angeles
 c=116.39#the longitude of Haining
-d=a-b #Calculate	the	longitude	distance	that	Rob	travelled	to	Los	Angeles
-e=c-a #Calculate	the	longitude	distance	that	Rob	travelled	to	Haining
+d=a-b #Calculate the longitude distance that Rob travelled to Los Angeles
+e=c-a #Calculate the longitude distance that Rob travelled to Haining
 x=d>e#whether or not the longitude distance that Rob travels to Los Angeles is longer than to Haining
 print("Is the longitude distance that Rob travels to Los Angeles longer than to Haining?")
 print(x)#the output will be true or false
@@ -15,7 +15,7 @@ print(x)#the output will be true or false
 x=1>=1#1>=1 is true
 y=5>6#5>6 is false
 w=x and y#true and false = false
-z=x or y#true or false=true
+z=x or y#true or false = true
 print(w)#print false
 print(z)#print true
 
