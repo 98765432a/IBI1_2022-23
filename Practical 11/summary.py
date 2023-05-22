2e43503e9b1479e52c001f71731031ea6044d03d
98765432a <127280386+98765432a@users.noreply.github.com>
2023-05-22 21:26:43 +0800

    Summary.py

--- Practical 11/summary.py
+++ Practical 11/summary.py
@@ -5,4 +5,4 @@ the alignment score of human and cat is:3717
 the percentage identity between human and mouse is:82.11%
 the percentage identity between cat and mouse is:81.74%
 the percentage identity between human and cat is:85.22%
-The human and cat ACE2 protein sequences are the most similar. During the evolution, the divergence of human and mouse ACE2 proteins is prior to the divergence of human and cat ACE2 proteins. the more recently a species diverged, the more similar their genomes.So the difference of ACE2 protein between human and mouse is higher than human and cat. 
+The human and cat ACE2 protein sequences are the most similar. During the evolution, the divergence of human and mouse ACE2 proteins is prior to the divergence of human and cat ACE2 proteins. The more recently a species diverged, the more similar their genomes. So the difference of ACE2 protein between human and mouse is higher than human and cat. 
