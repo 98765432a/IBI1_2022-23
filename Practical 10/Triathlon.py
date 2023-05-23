c4347a80b3e7fef507dbc3cdb1d041c7d27dd073
98765432a <127280386+98765432a@users.noreply.github.com>
2023-05-23 11:52:40 +0800

    Triathlon.py

--- Practical 10/Triathlon.py
+++ Practical 10/Triathlon.py
@@ -1,14 +1,15 @@
 class Triathlon (object):#import a class
 	def __init__(self, name, location, swim, cycle, run):#import a function
-		"""define the name, location,of athlete and their record of swim, cycle and run(assume them in the same unit)
-		add the scores together
-		print out the outcome"""
+		"""define the name, location,of athlete and their record of 
+		swim, cycle and run(assume them in the same unit)"""
 		self.name=name
 		self.location=location
 		self.swim=swim
 		self.cycle=cycle
 		self.run=run
 	def output(self):
+		"""add the scores together
+		return the outcome"""
 		total=self.swim+self.cycle+self.run#calculate the totol time cost of swim, cycle and run
 		return self.name, self.location, self.swim, self.cycle, self.run, total		
 c=Triathlon('Chris·White','Edinburgh', 30, 10, 8)#input data as an example
