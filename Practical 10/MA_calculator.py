c7d9dc88601608140c4252f77da0cab89826f456
98765432a <127280386+98765432a@users.noreply.github.com>
2023-05-23 11:50:38 +0800

    MA_calculator.py

--- Practical 10/MA_calculator.py
+++ Practical 10/MA_calculator.py
@@ -1,4 +1,4 @@
-def calculate(house,salary):
+def calculate(house,salary):#define a function as mortgage affordability calculator
 	"""calculate how many times the price of house is higher than the annual salary
         if the calculation is bigger than 5
         then we can buy the house"""
@@ -7,7 +7,7 @@ def calculate(house,salary):
 		print('Yes')
 	else:
 		print('No')
-c=calculate(180000,3500)#input house price and salary.for example,house price=180000,salary=35000,he/she can't bind the house
+c=calculate(180000,3500)#input house price and salary. For example,house price=180000,salary=35000,he/she can't bind the house
 #housep=input('please enter the house price here:')#input data
 #salarym=input('please enter your annual salary here:')#input data
 #c=Coordinate(int(housep),int(salarym))#change the input into intergers
